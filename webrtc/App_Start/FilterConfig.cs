a161702be547641951fe763a35dfdf076e43a9d5
Scott <smasherprog@gmail.com>
2017-07-07 06:20:12 -0700

    another project

---
+++ webrtc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace webrtc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ webrtc/Models/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebRTCExperiment.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+
+        public string Token { get; set; }
+
+        public string Name { get; set; }
+
+        public string SharedWith { get; set; }
+
+        public string Status { get; set; }
+
+        public System.DateTime LastUpdated { get; set; }
+
+        public string OwnerName { get; set; }
+
+        public string OwnerToken { get; set; }
+
+        public string ParticipantName { get; set; }
+
+        public string ParticipantToken { get; set; }
+    }
+    public class SDPMessage
+    {
+
+
+        public int ID { get; set; }
+
+        public string SDP { get; set; }
+
+        public bool IsProcessed { get; set; }
+
+        public string RoomToken { get; set; }
+
+        public string Sender { get; set; }
+    }
+
+    public class CandidatesTable
+    {
+
+        public int ID { get; set; }
+
+        public string Candidate { get; set; }
+
+        public string Label { get; set; }
+
+        public string RoomToken { get; set; }
+
+        public string Sender { get; set; }
+
+        public bool IsProcessed { get; set; }
+    }
+
+    public class WebRTCDataContext : DbContext
+    {
+        public WebRTCDataContext() : base("WebRTCData") { }
+        public DbSet<CandidatesTable> CandidatesTables { get; set; }
+        public DbSet<SDPMessage> SDPMessages { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+    }
+}
